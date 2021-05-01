7b325040b6f32a07e92cd7a275a425b0db1c5025
mangesh1404 <45553084+mangesh1404@users.noreply.github.com>
2021-05-01 10:36:59 +0530

    adding a new Project

---
+++ src/test/java/com/testcases/DemoTest.java
@@ -0,0 +1,21 @@
+package com.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void test01() {
+		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///C:/Users/Administrator/Desktop/Offline%20Website/Offline%20Website/index.html");
+		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.xpath("//button")).click();
+	}
+	
+	
+}
