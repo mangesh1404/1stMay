5571bb13b7dbd9840cee251faac319b94038b8ec
mangesh1404 <45553084+mangesh1404@users.noreply.github.com>
2021-05-15 09:46:59 +0530

    updated logger and listener function

--- src/test/java/com/testcases/DemoTest.java
+++ src/test/java/com/testcases/DemoTest.java
@@ -1,30 +1,24 @@
 package com.testcases;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class DemoTest {
+public class DemoTest extends TestBase {
 
+	
 	@Test
 	public void test01() {
-		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("file:///C:/Users/Administrator/Desktop/Offline%20Website/Offline%20Website/index.html");
+		driver = initialization();
 		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
 		driver.findElement(By.id("password")).sendKeys("123456");
 		driver.findElement(By.xpath("//button")).click();
+		Assert.assertEquals(driver.getTitle(), "JavaByKiran | Dashboard");
 	}
 	
 	@Test
 	public void test02() {
-		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("file:///C:/Users/Administrator/Desktop/Offline%20Website/Offline%20Website/index.html");
-		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
-		driver.findElement(By.id("password")).sendKeys("123456");
-		driver.findElement(By.xpath("//button")).click();
+		Assert.assertEquals(true, false);
 	}
 	
 }
--- src/test/java/com/testcases/DemoTest2.java
+++ src/test/java/com/testcases/DemoTest2.java
@@ -1,20 +1,23 @@
 package com.testcases;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class DemoTest2 {
+public class DemoTest2 extends TestBase {
 
 	@Test
 	public void test01() {
-		System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("file:///C:/Users/Administrator/Desktop/Offline%20Website/Offline%20Website/index.html");
+		driver = initialization();
 		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
 		driver.findElement(By.id("password")).sendKeys("123456");
 		driver.findElement(By.xpath("//button")).click();
+		Assert.assertEquals(driver.getTitle(), "JavaByKiran | Dashboard");
+	}
+	
+	@Test
+	public void test02() {
+		Assert.assertEquals(true, false);
 	}
 	
 	
